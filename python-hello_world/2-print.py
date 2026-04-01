f7387f4906a77670bd532c46ea9b9f412a1218d1
Sak1na-IT <pasayevasekine844@gmail.com>
2026-04-01 14:02:37 +0000

    python

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-# This script prints a specific string followed by a new line
-
-print("Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")
