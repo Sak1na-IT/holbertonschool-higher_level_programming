85e8c00a8a6400195f465d20c1c44a38d807421e
Sak1na-IT <pasayevasekine844@gmail.com>
2026-04-02 13:02:49 +0000

    python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming l"
 print(str[39:67] + "with " + str[0:6])
