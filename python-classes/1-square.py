cd91e6b4bb3ce31775d7e062e458d3447973f6fa
Sak1na-IT <pasayevasekine844@gmail.com>
2026-04-03 18:15:04 +0000

    python

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializes the square with a size
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
