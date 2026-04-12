c0b246f500d97aefcf88f7d3bf747e7225f5ad6e
Sak1na-IT <pasayevasekine844@gmail.com>
2026-04-12 17:21:38 +0000

    python

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""asdfghgsd"""
+
+
+def inherits_from(obj, a_class):
+    """asdfghyujik"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
