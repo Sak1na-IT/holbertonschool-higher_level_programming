71a0bd33e67185a38246a9674a1cf8d0eb0eed59
Sak1na-IT <pasayevasekine844@gmail.com>
2026-04-17 09:56:35 +0000

    python

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    asdfghjk
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
