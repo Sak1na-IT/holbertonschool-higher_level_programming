9d1e90b2a8581709827a5225a447fc6f106f3257
Sak1na-IT <pasayevasekine844@gmail.com>
2026-04-17 10:14:06 +0000

    python

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """
-esasdfghjk
+This module contains a function that returns an object
+represented by a JSON string.
 """
 import json
 
 
 def from_json_string(my_str):
     """
-    asdfghjasdfghjk 
+    Returns a Python object represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to deserialize.
+
+    Returns:
+        any: The Python data structure (list, dict, etc.).
     """
     return json.loads(my_str)
